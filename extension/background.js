8768a4bc2f51a86d571caa78e04050dd237ddd6f
PeterD1524 <qaz246135@gmail.com>
2020-11-03 00:53:09 +0800

    Add files via upload

---
+++ extension/background.js
@@ -0,0 +1,68 @@
+chrome.contextMenus.create(
+    {
+        "id": "Copy link text",
+        "title": "Copy link text",
+        "contexts": ["link"],
+    },
+    function () {
+        if ("lastError" in chrome.runtime) {
+            console.log("chrome.runtime.lastError.message", chrome.runtime.lastError.message);
+        }
+    }
+);
+chrome.contextMenus.create(
+    {
+        "id": "Copy image as data URI",
+        "title": "Copy image as data URI",
+        "contexts": ["image"],
+    },
+    function () {
+        if ("lastError" in chrome.runtime) {
+            console.log("chrome.runtime.lastError.message", chrome.runtime.lastError.message);
+        }
+    }
+);
+chrome.contextMenus.create(
+    {
+        "id": "Rotate clockwise",
+        "title": "Rotate clockwise",
+        "contexts": ["image", "video"],
+    },
+    function () {
+        if ("lastError" in chrome.runtime) {
+            console.log("chrome.runtime.lastError.message", chrome.runtime.lastError.message);
+        }
+    }
+);
+chrome.contextMenus.create(
+    {
+        "id": "Rotate counterclockwise",
+        "title": "Rotate counterclockwise",
+        "contexts": ["image", "video"],
+    },
+    function () {
+        if ("lastError" in chrome.runtime) {
+            console.log("chrome.runtime.lastError.message", chrome.runtime.lastError.message);
+        }
+    }
+);
+chrome.contextMenus.onClicked.addListener(function (info, tab) {
+    console.log(info);
+    console.log(tab);
+    let message = {};
+    message[info["menuItemId"]] = [];
+    let options = {};
+    if ("frameId" in info) {
+        options["frameId"] = info["frameId"];
+    }
+    chrome.tabs.sendMessage(tab["id"], message, options, function (response) {
+        if (arguments.length === 0) {
+            console.log("chrome.runtime.lastError.message", chrome.runtime.lastError.message);
+        } else {
+            console.log("response", response);
+            if ("chrome.runtime.lastError.message" in response) {
+
+            }
+        }
+    });
+});
---
+++ extension/contentScript.js
@@ -0,0 +1,79 @@
+let contextmenuElement = undefined;
+document.addEventListener("contextmenu", function (event) {
+    contextmenuElement = event.target;
+}, true);
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+    let element = contextmenuElement;
+    console.log(sender);
+    console.log(message);
+    if (element === undefined) {
+        alert("Failed :(");
+    } else {
+        console.log(element.tagName);
+        let requests = Object.keys(message);
+        if (Object.keys(message).length == 1) {
+            let request = requests[0];
+            if (request === "Copy link text") {
+                if (element.textContent !== "") {
+                    navigator.clipboard.writeText(element.textContent).then(function () {
+                        /* clipboard successfully set */
+                        console.log("/* clipboard successfully set */");
+                    }, function () {
+                        /* clipboard write failed */
+                        alert("/* clipboard write failed */");
+                        console.log("/* clipboard write failed */");
+                    });
+                }
+            } else if (request === "Copy image as data URI") {
+                if (element.tagName === "IMG") {
+                    fetch(new Request(element.src)).then(function (response) {
+                        console.log(response);
+                        response.blob().then(function (blob) {
+                            let reader = new FileReader();
+                            reader.addEventListener("load", function () {
+                                let dataURI = reader.result;
+                                navigator.clipboard.writeText(dataURI).then(function () {
+                                    /* clipboard successfully set */
+                                    console.log("/* clipboard successfully set */");
+                                }, function () {
+                                    /* clipboard write failed */
+                                    alert("/* clipboard write failed */");
+                                    console.log("/* clipboard write failed */");
+                                });
+                            });
+                            reader.readAsDataURL(blob);
+                            // let image = new Image();
+                            // image.addEventListener("load", function (event) {
+                            //     let canvas = document.createElement("canvas");
+                            //     canvas.width = image.width;
+                            //     canvas.height = image.height;
+                            //     let ctx = canvas.getContext("2d");
+                            //     ctx.drawImage(image, 0, 0);
+                            //     let dataURI = canvas.toDataURL(blob.type, 1);
+                            //     navigator.clipboard.writeText(dataURI).then(function () {
+                            //         /* clipboard successfully set */
+                            //         console.log("/* clipboard successfully set */");
+                            //     }, function () {
+                            //         /* clipboard write failed */
+                            //         alert("/* clipboard write failed */");
+                            //         console.log("/* clipboard write failed */");
+                            //     });
+                            // });
+                            // image.src = URL.createObjectURL(blob);
+                        });
+                    });
+                }
+            } else if (request === "Rotate clockwise") {
+                if (element.tagName === "IMG" || element.tagName === "VIDEO") {
+                    element.style.transform += " rotate(90deg)";
+                }
+            } else if (request === "Rotate counterclockwise") {
+                if (element.tagName === "IMG" || element.tagName === "VIDEO") {
+                    element.style.transform += " rotate(-90deg)";
+                }
+            }
+        } else {
+
+        }
+    }
+});
